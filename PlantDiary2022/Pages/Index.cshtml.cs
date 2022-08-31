2d7a9ddd987fa5486e89ac539dbbf3290468b178
Brandan <brandan.jones@uc.edu>
2022-08-30 23:17:38 -0400

    Consume a JSON feed from the Internet, and show the results in a C# Razor Page. See how it's done by watching this video: https://somup.com/c3jweDU2IU

--- PlantDiary2022/Pages/Index.cshtml.cs
+++ PlantDiary2022/Pages/Index.cshtml.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyPlantDiary;
+using System.Net;
 
 namespace PlantDiary2022.Pages
 {
     public class IndexModel : PageModel
     {
+
+        static readonly HttpClient client = new HttpClient();
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -22,6 +27,19 @@ namespace PlantDiary2022.Pages
             }
             int yearStarted = 2006;
             ViewData["Brand"] = brand + " Established " + yearStarted;
+
+            var task = client.GetAsync("http://plantplaces.com/perl/mobile/specimenlocations.pl?Lat=39.14455&Lng=-84.50939&Range=0.5&Source=location");
+            HttpResponseMessage result = task.Result;
+            List<Specimen> specimens = new List<Specimen>();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                specimens = Specimen.FromJson(jsonString);
+            }
+            ViewData["Specimens"] = specimens;
+            
+
         }
     }
 }
